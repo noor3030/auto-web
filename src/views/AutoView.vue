c19b37eda122c180290e6259227d6be7dd0afa54
noor3030 <nnorkrka@gmail.com>
2022-05-17 15:45:18 +0300

    cards componenets

--- src/views/AutoView.vue
+++ src/views/AutoView.vue
@@ -2,13 +2,7 @@
   <div>
     <v-container class="nav pa-7" fluid>
       <v-row justify="space-between">
-        <v-col cols="5">
-          <v-img
-            src="../assets/Group 7281.svg"
-            width="400"
-            class="phone-img"
-          ></v-img>
-        </v-col>
+     
         <v-col cols="5">
           <v-container class="container1 py-7">
             <v-row justify="center"
@@ -29,83 +23,39 @@
             <v-btn color="#fff" class="pa-6">Download the app</v-btn></v-row
           >
         </v-col>
+           <v-col cols="5">
+          <v-img
+            src="../assets/Group 7281.svg"
+            width="400"
+            class="phone-img"
+          ></v-img>
+        </v-col>
       </v-row>
       <v-row>
         <div>
-          <v-btn color="#276ef1" dark absolute bottom right fab large>
+          <v-btn color="#276ef1" dark absolute bottom left fab large>
             <v-icon>mdi-phone</v-icon>
           </v-btn>
         </div></v-row
       >
       <v-row>
-        <div class="services-div" style="float: right">
+        <div class="services-div " style="float: right">
           <h1>Our services</h1>
         </div></v-row
       >
-      <v-row class="pl-9" justify="space-between"
-        ><v-card elevation="10">
-          <v-card-actions class="justify-center">
-            <v-img src="../assets/2.svg" width="300"></v-img>
-          </v-card-actions>
-
-          <v-card-title class="justify-center">
-            <p style="font-weight: bold">24 hours 7 days in the week</p>
-          </v-card-title>
-
-          <v-card-text class="text-center">
-            <p style="font-size: 15px; color: #8691a1; font-weight: bold">
-              Auto with you in any time
-            </p>
-            <p style="font-size: 15px; color: #8691a1; font-weight: bold">
-              We fully support you in the problem you are facing
-            </p>
-          </v-card-text>
-        </v-card>
-        <v-card elevation="10">
-          <v-card-actions class="justify-center">
-            <v-img src="../assets/3.svg" width="300"></v-img>
-          </v-card-actions>
-
-          <v-card-title class="justify-center">
-            <p style="font-weight: bold">Your life is easier</p>
-          </v-card-title>
-
-          <v-card-text class="text-center">
-            <p style="font-size: 15px; color: #8691a1; font-weight: bold">
-              For the first time in Iraq
-            </p>
-            <p style="font-size: 15px; color: #8691a1; font-weight: bold">
-              Auto services find the solution in a button
-            </p>
-          </v-card-text>
-        </v-card>
-        <v-card elevation="10">
-          <v-card-actions class="justify-center">
-            <v-img src="../assets/1.svg" width="300"></v-img>
-          </v-card-actions>
-
-          <v-card-title class="justify-center">
-            <p style="font-weight: bold">Be safe</p>
-          </v-card-title>
-
-          <v-card-text class="text-center">
-            <p style="font-size: 15px; color: #8691a1; font-weight: bold">
-              Auto is the best choice
-            </p>
-            <p style="font-size: 15px; color: #8691a1; font-weight: bold">
-            To solve the problem without fatigue
-            </p>
-          </v-card-text>
-        </v-card>
-      </v-row>
+     <CardServices />
     </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-
-export default Vue.extend({});
+import CardServices from "@/components/CardServices.vue"
+export default Vue.extend({
+  components:{
+    CardServices
+  }
+});
 </script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Lora&family=Montserrat+Alternates&family=Nunito+Sans:wght@200&family=Orelega+One&family=Outfit&family=Tajawal:wght@500&display=swap");
@@ -142,4 +92,5 @@ p {
   padding-top: 50px;
   padding-bottom: 50px;
 }
+
 </style>
